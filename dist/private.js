5561b5340da67df8848b5983f67b08f5a1771d86
JinCheng <jinchengjoker@foxmail.com>
2020-06-16 11:51:11 +0800

    delete /dist

--- dist/private.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// 注意：暴露appSecret，有被盗用造成损失的风险
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.key = exports.appKey = void 0;
-var appKey = '4f5bbcf4b6d3b8e0';
-exports.appKey = appKey;
-var key = 'XIOZ7tIkdeo0C5UuTOSTZ3zCAj6cZy8A';
-exports.key = key;
